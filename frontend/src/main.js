bdcd72df66893eccf3029cee6b89a20dc740f909
akifsah <04makifsahin@proton.me>
2025-04-09 17:53:19 +0300

    Add pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,20 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import './style.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-// Import specific icons
 import { faBell as faBellSolid } from '@fortawesome/free-solid-svg-icons'
 import { faBell as faBellRegular } from '@fortawesome/free-regular-svg-icons'; // Regular (outline) bell icon
 
-// Add icons to the library
 library.add(faBellSolid, faBellRegular)
 
 const app = createApp(App)
 
-// Register the component globally
 app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(createPinia())
 
 app.mount('#app')
 
